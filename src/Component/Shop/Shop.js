a7e76b6fe80cb86a8743ead6f3421f17ea6fe0ef
sikderp-github <66568657+sikderp-github@users.noreply.github.com>
2020-08-25 21:59:43 +0600

    ema-john-simple final

--- src/Component/Shop/Shop.js
+++ src/Component/Shop/Shop.js
@@ -2,13 +2,17 @@ import React, { useState } from 'react';
 import fakeData from '../../fakeData';
 import './Shop.css'
 import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
 
 
 const Shop = () => {
     const first10 = fakeData.slice(0, 10);
     const [products, setProducts] = useState(first10);
+    const [cart, setCart] = useState([]);
+
     const handleAddProducts = (product) => {
-        console.log('click added', product)
+        const newCart = [...cart, product];
+        setCart(newCart);
     }
     return (
         <div className='shop-container'>
@@ -20,7 +24,7 @@ const Shop = () => {
 
             </div>
             <div className="cart-container">
-                <h3>This is cart.</h3>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
